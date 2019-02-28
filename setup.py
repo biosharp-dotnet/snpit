2c11ddd3e250e5160f05fa0eeca5ec7511f0e353
thobalose <lose.thoba@gmail.com>
2019-02-28 09:02:53 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='snpit',
     version='1.0.0',
     author='Samuel Lipworth',
+    url='https://github.com/samlipworth/snpit',
+    email='samuel.lipworth@medsci.ox.ac.uk',
     packages=['snpit'],
     install_requires=[
         "PyVCF >= 0.6.8",
         "biopython >= 1.70"
     ],
     scripts=["bin/snpit-run.py"],
-    license='unknown',
-    long_description=open('README.md').read(),
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
