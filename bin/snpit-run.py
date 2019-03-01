e731f51bb0a7484bf2bf8ba4d815abceded5f164
thobalose <lose.thoba@gmail.com>
2019-03-01 10:20:55 +0200

    Refactored snpit-run.py

--- bin/snpit-run.py
+++ bin/snpit-run.py
@@ -1,32 +1,40 @@
 #! /usr/bin/env python
 
-#programme to report the lineage/subspecies of a TB sample alligned to NC000962
-#useage: python SNP-IT.py [guuid] [name of outfile]
-#or for lots of samples: cat [list of samples] | parallel -j[no. of threads] python fasta_typer9.py {} {}.out
-#Output1: is to file containing absolute and % hits for all subspecies
-#Output2: only the top call, is to standard out - redirect to file if you want eg 1>calls.log
-import vcf
+import argparse
 
 from snpit import snpit
 
-import argparse
+# programme to report the lineage/subspecies of a TB sample alligned to NC000962
+# usage: python SNP-IT.py [guuid] [name of outfile]
+# or for lots of samples: cat [list of samples] | parallel -j[no. of threads] python fasta_typer9.py {} {}.out
+# Output1: is to file containing absolute and % hits for all subspecies
+# Output2: only the top call, is to standard out - redirect to file if you want eg 1>calls.log
+
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input",required=True,help="the path to the VCF or FASTA file to read and classify (either can be bzip2ed/gzipped)")
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--threshold", default=10, type=int,
+                        help="the percentage of snps above which a sample is considered to belong to a lineage")
+    parser.add_argument("--input", required=True,
+                        help="the path to the VCF or FASTA file to read and classify (either can be bzip2ed/gzipped)")
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s v1.1')
     options = parser.parse_args()
 
-    # now try opening the specified input file
+    # try opening the specified input file
     try:
-        TEST = open(options.input,'r')
-    except IOError:
-        print("input file "+options.input+" does not exist!")
-
-    # create an instance (this loads all the lineages)
-    tb=snpit(threshold=10,input_file=options.input)
-
-    if tb.percentage is not None:
-        print("%s\t%s\t%s\t%s%.1f %%" % (options.input,tb.species,tb.lineage,tb.sublineage,tb.percentage))
+        input_file = open(options.input, 'r')
+    except OSError as e:
+        raise OSError(
+            "--input {} does not exist!\n{}".format(options.input, e))
     else:
-        print("%s\t%s" % (options.input,"none identified"))
+        input_file.close()
+        # create an instance (this loads all the lineages)
+        tb = snpit(threshold=options.threshold, input_file=options.input)
+        if tb.percentage is not None:
+            print("%s\t%s\t%s\t%s%.1f %%" % (
+                options.input, tb.species, tb.lineage, tb.sublineage, tb.percentage))
+        else:
+            print("%s\t%s" % (options.input, "none identified"))
